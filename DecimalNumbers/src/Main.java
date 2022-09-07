6d9ae77093b3c60c8774f68340985fa25eb955aa
Ashutosh Kashyap <ashutosh.kashyap.ak97@gmail.com>
2022-09-07 20:49:49 +0530

    Java 3: String basic Introduction

--- DecimalNumbers/src/Main.java
+++ DecimalNumbers/src/Main.java
@@ -5,21 +5,35 @@ public class Main {
         // System.out.println(myMinFloatValue);
         // System.out.println(myMaxFloatValue);
 
-        double enterPound = 200d;
-        double Kg = enterPound * 0.45359237d ;
-        System.out.println(Kg);
+        //double enterPound = 200d;
+        //double Kg = enterPound * 0.45359237d ;
+        //System.out.println(Kg);
 
-        double format = 3_000_987.5_567_324d;
-        System.out.println(format);
+        //double format = 3_000_987.5_567_324d;
+        //System.out.println(format);
         // note: when precise calculations are performed we use BigDecimal
 
-        char myChar = 'A';
+        //char myChar = 'A';
         //65535 charactes in unicode
-        char UnicodeChar = '\u0088';
-        System.out.println(UnicodeChar);
+        //char UnicodeChar = '\u0088';
+        //System.out.println(UnicodeChar);
 
-        boolean myTrue = true;
+        //boolean myTrue = true;
         // use 'is' to name the variable
-        boolean isCustomer = false;
+        //boolean isCustomer = false;
+
+        // String is not a primitive data type. It is a class and have properties and functions
+        String myString = "Ashu";
+        System.out.println(myString);
+        String numberString = "250.20";
+        String secondNumber = "22.80";
+        System.out.println(numberString+secondNumber);
+
+        int thirdNumber = 10;
+        System.out.println(secondNumber+thirdNumber);
+
+        //String in java is immutable we cannot change the string
+        //StringBuffer is used instead
+
     }
 }
